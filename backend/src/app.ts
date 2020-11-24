11cdb70f741e94bc1e94ea202fd70cf17fd347c4
JoseJuniorr <jjerrorama@gmail.com>
2020-11-24 15:05:01 -0300

    add cors

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,13 +2,15 @@ import express from "express";
 
 import LinksRouter from "./routes/links.routes";
 
+import cors from "cors";
+
 const app = express();
 
 //Configurações
 app.set("port", process.env.PORT || 4000);
 
 app.use(express.json());
-
+app.use(cors());
 app.use(LinksRouter);
 
 export default app;
